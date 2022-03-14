5aae047ea3432e8fc7332b7f647da8ec8b3e7433
jfawcet5 <joshfawcettshs10@gmail.com>
2022-03-14 11:56:29 -0700

    Removed old unused files

--- testSendToServer.py
+++
@@ -1,9 +0,0 @@
-
-from socket import *
-serverIP = '54.160.235.144'
-cSock = socket(AF_INET, SOCK_STREAM)
-cSock.connect((serverIP, 12000))
-hostname = gethostname()
-self_ip = gethostbyname(hostname)
-cSock.send(self_ip.encode())
-cSock.send('10.185.236.190\r\nHello this is a test'.encode())
